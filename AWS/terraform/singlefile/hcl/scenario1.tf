fe5423e45b4c7812c8ddc9b2816b138040f5c356
mateescu <mateescu@ca.ibm.com>
2018-02-25 18:58:10 -0500

    default values

--- AWS/terraform/singlefile/hcl/scenario1.tf
+++ AWS/terraform/singlefile/hcl/scenario1.tf
@@ -24,10 +24,12 @@ variable "aws_secret_key" {
 
 variable "vpc_name_tag" {
   description = "Name of the Virtual Private Cloud (VPC) this resource is going to be deployed into"
+  default = "CAMVPC"
 }
 
 variable "subnet_cidr" {
   description = "Subnet cidr"
+  default = "10.0.0.0/24"
 }
 
 data "aws_vpc" "selected" {
@@ -51,14 +53,17 @@ data "aws_security_group" "selected" {
 
 variable "public_ssh_key_name" {
   description = "Name of the public SSH key used to connect to the virtual guest"
+  default = "radu_test_key"
 }
 
 variable "public_ssh_key" {
   description = "Public SSH key used to connect to the virtual guest"
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDHZ4niF/AJ/9y+/NCqXAnr+rWYEg9kv1/4uOc0PEQU+HBelUNM7RxthH5VGYPPEm9KuVBhwlO7VQ1Kpo2SrkdjX16YtM1Ozao4DyU7jJNh3PpvzjzPhqWf6lKo1AQ5EX4HBkIGe+G9zc17p6op8ZUniWNMux9zv3jf2F1zuMt/gbLGUmzUOUWU5lIwqEZPfToeJJhdQgpqFTYqfxhvKSkj0HTGOz8UbEkP37TIqx0pjohZjoIgbZDIz1YkfX52MaSEiHg7WdBAZiaJxFa1mAAwL5vZNg80/lSsKa96ga+INw1b0NHeEVVVWWBxy4Vo9n/1NPaYu1A/dYFoF5huEKep developer@radu-orpheus"
 }
 
 variable "security_group_id" {
   description = "security_group"
+  default = "sg-3cbeec40"
 }
 
 
