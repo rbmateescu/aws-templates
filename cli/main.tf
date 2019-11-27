b35c1f80e6bec93ad51b9a23ead15df28b3b760d
mateescu <mateescu@ca.ibm.com>
2019-11-27 10:40:06 -0500

    aws

--- cli/main.tf
+++ cli/main.tf
@@ -1,6 +1,6 @@
 
 resource "null_resource" "create-endpoint" {
   provisioner "local-exec" {
-    command = "source /home/terraform/.bashrc && aws --version "
+    command = "aws2 --version"
   }
 }
